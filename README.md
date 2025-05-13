b48e0f1bf19ad2f936a8ac44604f675a118f67be
@Colline <2samax24@gmail.com>
2025-05-13 15:30:05 +0600

    Model Evaluation step completed

--- README.md
+++ README.md
@@ -13,3 +13,56 @@ MLflow End-to-End Machine learning practice
 8. Update the main.py 
 9. Update the dvc.yaml
 10. Update app.py  
+
+
+# How to run this Project 
+1. ### STEPS 
+
+Clone the repository 
+
+---bash 
+https://github.com/samaTech-sys/End-to-End-Machine-Learning-with-Mlflow
+---
+
+2. ### STEP TWO 
+Create a vitual environment using conda after opeing the repository and activate it 
+
+---bash 
+conda create -n mlproject python=3.10 -y
+---
+
+---bash 
+conda activate mlproject
+----
+
+3. ## STEP THREE
+Install requirements 
+---bash 
+pip install requirements -r requiremnts. text
+----
+
+4. ## STEP FOUR 
+Finally run the following command 
+---bash 
+python app.py 
+----
+
+Now, 
+Open up your local host and port 
+
+## MLFLOW 
+Go through mlflow documenetation 
+export W_TRACKING_URL=https://dagshub.com/samaTech-sys/End-to-End-Machine-Learning-with-Mlflow.mlflow
+export MLFLOW_TRACKING_USERNAME=SamaTech-sys
+export MLFLOW_TRACKING_PASSWORD=27c279e0895284a35a6198b88076b93c13073cab
+python script.py 
+## 
+[Documentation](https://www.mlflow.org/docs/1.29.0/index.html)
+mlflow ui 
+
+Dagshub 
+[dagshub](http://dagshub.com)
+
+
+
+
--- setup.py
+++ setup.py
@@ -2,28 +2,26 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-    
-    __version__ = "0.0.0"
-    
-    REPO_NAME = "End-to-End-MAchine-Learning-with-MLflow"
-    AUTHOR_USER_NAME = "colline"
-    SRC_REPO = "mlflow"
-    AUTHOR_EMAIL = "collinetazuba@gmail.com"
-    
-    setuptools.setup(
-        name=SRC_REPO, 
-        version=__version__,
-        author=AUTHOR_USER_NAME,
-        author_email =AUTHOR_EMAIL,
-        description = "A small python project for ml app", 
-        long_description=long_description,
-        long_description_content_type ="text/markdown",
-        url= f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}", 
-        project_urls = {
-            "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues", 
- 
-        }, 
-        package_dir= {"":"src"},
-        packages= setuptools.find_packages(where="src")
-    )
-        
+
+__version__="2.9.2"
+
+REPO_NAME = "End-to-End-MAchine-Learning-with-MLflow"
+AUTHOR_USER_NAME = "colline"
+SRC_REPO = "test_mlflow" #here back
+AUTHOR_EMAIL = "collinetazuba@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,  # ✅ Avoid conflict with the real mlflow package
+    version=__version__,  # ✅ Add a version to your project
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python project for ML app", 
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"http://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues", 
+    }, 
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
